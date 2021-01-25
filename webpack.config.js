4574bf2a97e02dfa6f6fe3ea3ded49cab6da4ee6
natalyamyunster <natalyamyunster@yahoo.com>
2021-01-25 16:18:13 +0100

    module2 lesson1

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,13 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
+const IS_PROD = NODE_ENV === 'production';
+
+function setupDevtool(){
+    if(IS_DEV) return 'eval';
+    if(IS_PROD) return false;
+
+}
 
 module.exports = {
     resolve: {
@@ -27,5 +34,6 @@ module.exports = {
         port: 3000,
         open: true,
         hot: IS_DEV
-    }
+    },
+    devtool: setupDevtool()
 };
