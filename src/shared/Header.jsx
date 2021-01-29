b648a11ac41b2c51113af55b93b68c1e32892595
natalyamyunster <natalyamyunster@yahoo.com>
2021-01-29 11:45:36 +0100

    module2 lesson3

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,11 +1,21 @@
-import * as React from 'react';
+import  React, {useState} from 'react';
+import { hot } from 'react-hot-loader/root';
 
-export function Header(){
+function HeaderComponent(){
+    const [state, setState] = useState(false);
     return (
         <header>
-            <h1>
-                Reddit for our own 123
-            </h1>
+            <h1>HMR Example</h1>
+            <button onClick={() => setState(!state)}>
+                {state ? 'hide' : 'show'}
+            </button>
+            {state && <h3> Here I'm here 123</h3>}
+            <p>
+                It replace code without page reloading
+            </p>
+            <b>I edit code now</b>
         </header>
     )
 }
+
+export const Header = hot(HeaderComponent);
