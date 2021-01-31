7fdd3560fb3020829d36d21305ceb6b4a35b041c
natalyamyunster <natalyamyunster@yahoo.com>
2021-01-31 07:00:27 +0100

    module2 almost fixed (npm run dev), hook is commented

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,17 +1,17 @@
-import  React, {useState} from 'react';
+import * as  React from 'react';
 import { hot } from 'react-hot-loader/root';
 
 const HeaderComponent = () => {
 
-    const [state, setState] = useState(false);
+    // const [state, setState] = useState(false);
 
     return (
         <header>
             <h1>HMR - Hot Module Replacement Example</h1>
-            <button onClick={() => setState(!state)}>
-                {state ? 'hide' : 'show'}
-            </button>
-            {state && <h3> Here I'm here 123</h3>}
+            {/*<button onClick={() => setState(!state)}>*/}
+            {/*    {state ? 'hide' : 'show'}*/}
+            {/*</button>*/}
+            {/*{state && <h3> Here I'm here 123</h3>}*/}
             <p>
                 It replace code without page reloading 123
             </p>
