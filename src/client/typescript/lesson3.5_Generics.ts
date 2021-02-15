ad089f83a14dd89269c2182d6f5bf6c8f9dafc1a
natalyamyunster <natalyamyunster@yahoo.com>
2021-02-15 17:39:42 +0100

    module3 lesson 5 ts

--- src/client/typescript/lesson3.5_Generics.ts
+++ src/client/typescript/lesson3.5_Generics.ts
@@ -1,10 +1,14 @@
 //Generics
 const myArray: MyArray<number> = [1, 2, 3];
 
-interface MyArray <T>{
+interface MyArray <T> {
     [N: number]: T
+    map<U>(fn: (el: T) => U): U[]
 }
 
 let variable = myArray[1];
+myArray.map(f => f + 1);
+myArray.map(f => `f + ${f}`);
+
 [1, 2, 3].map(f=>f + 1); // [2, 3, 4]
 [1, 2, 3].map(f=>`f + ${f}`); // ['f+1', 'f + 2, 'f + 3']
