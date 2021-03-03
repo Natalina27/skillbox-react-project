15f9f39b04dea50f2bb83c3faed2f0daaa5cf311
natalyamyunster <natalyamyunster@yahoo.com>
2021-03-03 09:23:51 +0100

    module3 lesson 6 ts classes

--- src/client/typescript/lesson3.6_Classes.tsx
+++ src/client/typescript/lesson3.6_Classes.tsx
@@ -11,6 +11,8 @@
 // instance.method(); // 123
 
 //New
+import React from 'react';
+
 class Constructor{
     public field: number = 123;
     private privateField: number = 123456;
@@ -56,13 +58,40 @@ abstract class AbstractClass{
     protected protectedMethod(){
         return this.abstractField;
     }
-};
+}
 
-class NewChild extends AbstractClass{
-    public abstractField: number = 123 ;
+class NewChild extends AbstractClass {
+    public abstractField: number = 123;
 
     public abstractMethod(): number {
-        return 0;
+        return this.protectedMethod();
     }
+}
 
+interface MyInterface<T> {
+    field:string;
+    method(): string;
+}
+
+class NewClass<T> implements MyInterface<T> {
+    public field: string = '123';
+    public conf?: T;
+
+    public method(): string {
+        return '';
+    }
+}
+
+class MyComponent extends React.Component<{prop1: number}, {state1: string}>{
+    constructor(props: {prop1: number}){
+        super(props);
+        this.state = {
+            state1: '123'
+        }
+    }
+    public render(){
+        return (
+            <div> { this.props.prop1}</div>
+        )
+    }
 }
